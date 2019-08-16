87043530c38d727954e1a245f480d538151d4819
yamotuki <mamemochi3ko@gmail,com>
2019-08-16 15:46:59 +0800

    fetchのsleepをoffにする

--- index.php
+++ index.php
@@ -294,7 +294,7 @@ $app->get('/fetch', function (Request $request, Response $response) {
         return $response->withStatus(403);
     }
 
-    sleep(1);
+ #   sleep(1);
 
     $dbh = getPDO();
     $stmt = $dbh->query('SELECT id FROM channel');
